f88cbf7bad2acd6167b777334966d88ad1a36d1f
Hangchen Yu <yuhc123@gmail.com>
2015-10-01 23:45:23 -0500

    Update template

---
+++ template/lc.c
@@ -0,0 +1,17 @@
+/* LC ID :  #8
+ * Type  :  
+ * Author:  Hangchen Yu
+ * Date  :  09/15/2015
+ */
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <math.h>
+
+//copy the Solution class here
+
+int main() {
+
+    return 0;
+}
