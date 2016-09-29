24a3bacfbd900d5c2d30810e6c293dd9e0dc07ae
Hangchen Yu <yuhc123@gmail.com>
2016-09-28 22:08:27 -0500

    Add solution to LC193

---
+++ src/lc193.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Read from the file file.txt and output all valid phone numbers to
+# stdout.
+grep -e '\(^[0-9]\{3\}-[0-9]\{3\}-[0-9]\{4\}$\)' \
+     -e '\(^([0-9]\{3\})\s[0-9]\{3\}-\([0-9]\{4\}\)$\)' file.txt
